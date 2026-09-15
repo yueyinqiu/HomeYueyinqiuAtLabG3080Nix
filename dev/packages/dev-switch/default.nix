016671582fa5a5ec7b223450f4936f0c05da479a
yueyinqiu <yueyinqiu@outlook.com>
2026-09-15 15:27:24 +0000

    x

--- dev/packages/dev-switch/default.nix
+++ dev/packages/dev-switch/default.nix
@@ -3,6 +3,6 @@
 pkgs.writeShellApplication {
   name = name;
   text = ''
-    home-manager switch --flake .
+    home-manager switch --flake . --extra-experimental-features 'nix-command flakes'
   '';
 }
