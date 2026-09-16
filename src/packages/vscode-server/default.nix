f7f7c915a79d23a0db159c12f3d7b3f1ffbea99e
yueyinqiu <yueyinqiu@outlook.com>
2026-09-16 08:22:13 +0000

    vsc?

---
+++ src/packages/vscode-server/default.nix
@@ -0,0 +1,11 @@
+{ vscode-server, ... }: {
+  imports = [
+    vscode-server.homeModules.default
+  ];
+          
+  services.vscode-server.enable = true;
+  services.vscode-server.enableFHS = true;
+  services.vscode-server.extraRuntimeDependencies = pkgs: [
+    pkgs.nixd
+  ];
+}
