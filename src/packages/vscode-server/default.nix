e3e01eed3b345ea40eb43f5787d9ab001300c217
yueyinqiu <yueyinqiu@outlook.com>
2026-09-16 08:22:55 +0000

    ?

--- src/packages/vscode-server/default.nix
+++ src/packages/vscode-server/default.nix
@@ -5,7 +5,7 @@
           
   services.vscode-server.enable = true;
   services.vscode-server.enableFHS = true;
-  services.vscode-server.extraRuntimeDependencies = pkgs: [
+  services.vscode-server.extraRuntimeDependencies = [
     pkgs.nixd
   ];
 }
