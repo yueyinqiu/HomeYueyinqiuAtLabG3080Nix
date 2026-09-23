23def71436419f4ee29de4fd2fec80160d812955
yueyinqiu <yueyinqiu@outlook.com>
2026-09-23 12:54:11 +0000

    try

--- src/packages/nh/default.nix
+++ src/packages/nh/default.nix
@@ -1,7 +1,8 @@
-{ ... }: {
+{ config, ... }: {
   programs.nh = {
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 7d --keep 5";
   };
+  systemd.user.services.nh-clean.path = [ "${config.home.homeDirectory}/.nix-profile/bin" ];
 }
