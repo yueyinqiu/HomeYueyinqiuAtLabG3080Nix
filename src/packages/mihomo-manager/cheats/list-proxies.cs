b49d3d571d9ccaed6988971e22b5e9a587a3b718
yueyinqiu <yueyinqiu@outlook.com>
2026-09-24 07:46:32 +0000

    c

---
+++ src/packages/mihomo-manager/cheats/list-proxies.cs
@@ -0,0 +1,28 @@
+#:package Snavi.ArgumentSuggester@0.0.2
+#:package CliWrap@3.10.4
+
+using System.Runtime.CompilerServices;
+using CliWrap;
+using CliWrap.Buffered;
+using Snavi.ArgumentSuggester;
+
+await new Suggester().RunAsync();
+
+class Suggester : SnaviArgumentSuggester
+{
+    public override async IAsyncEnumerable<(string Value, string Description)> SuggestAsync(
+        IReadOnlyList<string> givenArguments,
+        DirectoryInfo currentDirectory,
+        DirectoryInfo temporaryDirectory,
+        [EnumeratorCancellation] CancellationToken cancellationToken
+    )
+    {
+        var output = await Cli.Wrap("mihomo-manager")
+            .WithArguments(["list"])
+            .ExecuteBufferedAsync(cancellationToken);
+        foreach (var instance in output.StandardOutput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        {
+            yield return (instance, "");
+        }
+    }
+}
--- src/packages/mihomo-manager/default.nix
+++ src/packages/mihomo-manager/default.nix
@@ -1,3 +1,16 @@
-{ ... }: {
+{ pkgs, ... }: {
   home-manager-mihomo-manager.enable = true;
+
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "m";
+      text = ''
+        exec mihomo-manager "$@"
+      '';
+    })
+  ];
+
+  imports = [
+    ./cheats.nix
+  ];
 }
